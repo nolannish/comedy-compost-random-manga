f42efc5facf402f2f9f899e00db139c3119c7d92
nolannish <nolannish@gmail.com>
2025-07-23 11:31:28 -0700

    update to getrandomint

--- comedy-compost-random-manga/src/app/api/jikan/route.ts
+++ comedy-compost-random-manga/src/app/api/jikan/route.ts
@@ -17,5 +17,5 @@ export async function GET(requst: NextRequest) {
 function getRandomInt(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max))
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
