1d3dcc1adeedb9b21afd8b24d5352b9d95be21b1
nolannish <nolannish@gmail.com>
2025-07-25 10:57:44 -0700

    Genres now appear on webpage

--- comedy-compost-random-manga/src/components/GenreDropdown.tsx
+++ comedy-compost-random-manga/src/components/GenreDropdown.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import { GetGenreOptions } from '@/app/data/genre-options';
 
-const options = await GetGenreOptions()
+type SelectOption = {
+  value: number;
+  label: string;
+}
 
 export default function GenreDropdown() {
+  const [options, setOptions] = useState<SelectOption[]>([]);
+
+  useEffect(() => {
+    async function getOptions() {
+      const genres = await GetGenreOptions();
+
+      const selectOptions = genres.map((genre) => ({
+        value: genre.mal_id,
+        label: genre.name
+      }));
+
+      setOptions(selectOptions);
+    }
+
+    getOptions();
+  }, []);
+
   return (
-    <Select options={options} />
+    <Select options={options} placeholder='Select a genre' />
   )
 }
