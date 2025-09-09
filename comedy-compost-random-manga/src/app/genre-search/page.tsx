ef81c82ac39ba6ef0ccbecbb2ba0f59392647e01
nolannish <nolannish@gmail.com>
2025-09-09 12:56:15 -0700

    update genre box page orientation

--- comedy-compost-random-manga/src/app/genre-search/page.tsx
+++ comedy-compost-random-manga/src/app/genre-search/page.tsx
@@ -96,12 +96,18 @@ export default function MangaPage() {
   return (
     <main className="min-h-screen flex flex-col items-center bg-gray-50 text-gray-800">
       <Header />
-      <h2>Include Genres</h2>
-      <GenreDropdown onChange={handleSelectionChange} options={options.filter(opt => !selectionsExclude.includes(opt.value))}/>
-      <h2>Exclude Genres</h2>
-      <GenreDropdown onChange={handleSelectionChangeExclude} options={options.filter(opt => !selections.includes(opt.value))}/>
-       <h1 className="text-3xl font-bold mb-4">Random Manga Finder</h1>
+      <h1 className="text-3xl font-bold mb-4">Random Manga Finder</h1>
       <p className="text-gray-600 mb-4">Select genres to include or exclude, then click &quot;Fetch Manga&quot;.</p>
+      <div className="flex flec-col gap-6 md:flex-row md:gap-12 items-start">
+        <div className="flex flex-col">
+          <h2>Include Genres</h2>
+          <GenreDropdown onChange={handleSelectionChange} options={options.filter(opt => !selectionsExclude.includes(opt.value))}/>
+        </div>
+        <div className="flex flex-col">
+          <h2>Exclude Genres</h2>
+          <GenreDropdown onChange={handleSelectionChangeExclude} options={options.filter(opt => !selections.includes(opt.value))}/>
+        </div>
+      </div>
       {loading && <p>Loading...</p>}
       {error && <p className="text-red-500">{error}</p>}
       
